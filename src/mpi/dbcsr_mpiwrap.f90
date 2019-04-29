162d781cf40d6670bb5d96244a34e31841bf49f5
Hans Pabst <hans.pabst@intel.com>
2019-04-29 17:14:02 +0200

    Attempt to avoid temporary array.

--- src/mpi/dbcsr_mpiwrap.f90
+++ src/mpi/dbcsr_mpiwrap.f90
@@ -999,7 +999,7 @@
 !> \note see mp_sum_${nametype1}$
 ! *****************************************************************************
   SUBROUTINE mp_sum_${nametype1}$v(msg, gid)
-     ${type1}$, INTENT(INOUT)                   :: msg(:)
+     ${type1}$, CONTIGUOUS, INTENT(INOUT)     :: msg(:)
      INTEGER, INTENT(IN)                      :: gid
 
      CHARACTER(len=*), PARAMETER :: routineN = 'mp_sum_${nametype1}$v', &

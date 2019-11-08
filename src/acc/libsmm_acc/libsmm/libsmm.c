12e5b70887202e34db84b3cb64b76411de8784ad
Hans Pabst <hans.pabst@intel.com>
2019-11-08 16:21:35 +0100

    Added placeholder code for OpenMP-based LIBSMM.

---
+++ src/acc/libsmm_acc/libsmm/libsmm.c
@@ -0,0 +1,38 @@
+/*------------------------------------------------------------------------------------------------*
+ * Copyright (C) by the DBCSR developers group - All rights reserved                              *
+ * This file is part of the DBCSR library.                                                        *
+ *                                                                                                *
+ * For information on the license, see the LICENSE file.                                          *
+ * For further information please visit https://dbcsr.cp2k.org                                    *
+ * SPDX-License-Identifier: GPL-2.0+                                                              *
+ *------------------------------------------------------------------------------------------------*/
+#include "libsmm.h"
+#include <stdlib.h>
+#include <assert.h>
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+int libsmm_acc_process(void* param_stack, int stack_size,
+  int nparams, int datatype, void* a_data, void* b_data, void* c_data,
+  int m_max, int n_max, int k_max, int def_mnk, void* stream)
+{
+  return EXIT_FAILURE;
+}
+
+int libsmm_acc_transpose(void* trs_stack, int offset, int nblks,
+  void* buffer, int datatype, int m, int n, void* stream)
+{
+  return EXIT_FAILURE;
+}
+
+int libsmm_acc_libcusmm_is_thread_safe(void)
+{
+  return EXIT_FAILURE;
+}
+
+#if defined(__cplusplus)
+}
+#endif

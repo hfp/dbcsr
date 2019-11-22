c4c61f327a9f6aefb56de70c774ed557b55d338f
Hans Pabst <hans.pabst@intel.com>
2019-11-22 12:07:39 +0100

    Further adjustments to fix compilation.

--- src/acc/libsmm_acc/libsmm_acc.h
+++ src/acc/libsmm_acc/libsmm_acc.h
@@ -40,13 +40,13 @@ struct kernel_launcher {
 
 static std::unordered_map<Triplet, kernel_launcher> kernel_handles;
 
-int libsmm_acc_process_d(int *param_stack, int stack_size,
+int libsmm_acc_process_d(const int *param_stack, int stack_size,
                          ACC_DRV(stream) stream, int m, int n, int k,
-                         double * a_data, double * b_data, double * c_data);
+                         const double * a_data, const double * b_data, double * c_data);
 
 static std::unordered_map<Triplet, ACC_DRV(function)> transpose_handles;
 
-int libsmm_acc_transpose_d(int *trs_stack, int offset, int nblks, double *buffer,
+int libsmm_acc_transpose_d(const int *trs_stack, int offset, int nblks, double *buffer,
                            int m, int n, ACC_DRV(stream) stream);
 
 #endif // LIBSMM_ACC_H

8d4ac0ea98082743545a4bc7dd26fd1c8d899e13
Hans Pabst <hans.pabst@intel.com>
2020-09-13 18:12:55 +0200

    Code cleanup.

--- src/acc/opencl/acc_opencl.c
+++ src/acc/opencl/acc_opencl.c
@@ -241,43 +241,38 @@ int acc_get_ndevices(int* n_devices)
 
 int acc_set_active_device(int device_id)
 {
-  cl_int result = EXIT_SUCCESS;
-  if (0 <= device_id && device_id < acc_opencl_ndevices) {
-    cl_device_id current_id = NULL;
-    size_t n = 0;
+  cl_int result = ((NULL != acc_opencl_context && 0 <= device_id && device_id < acc_opencl_ndevices)
+    ? EXIT_SUCCESS
+    : EXIT_FAILURE);
+  cl_device_id current_id = NULL;
+  size_t n = 0;
+  ACC_OPENCL_CHECK(clGetContextInfo(acc_opencl_context, CL_CONTEXT_DEVICES,
+    sizeof(cl_device_id), &current_id, &n), "failed to query current device id", result);
+  assert(EXIT_SUCCESS != result || sizeof(cl_device_id) == n/*single-device context*/);
+  if (acc_opencl_devices[device_id] != current_id) {
+    cl_context_properties properties[] = {
+      CL_CONTEXT_PLATFORM, (cl_context_properties)acc_opencl_platforms[device_id],
+      CL_CONTEXT_INTEROP_USER_SYNC, CL_FALSE, /* TODO */
+      0 /* end of properties */
+    };
     if (NULL != acc_opencl_context) {
-      ACC_OPENCL_CHECK(clGetContextInfo(acc_opencl_context, CL_CONTEXT_DEVICES,
-        sizeof(cl_device_id), &current_id, &n), "failed to query current device id", result);
-      assert(EXIT_SUCCESS != result || sizeof(cl_device_id) == n/*single-device context*/);
+      ACC_OPENCL_CHECK(clReleaseContext(acc_opencl_context),
+        "failed to release OpenCL context", result);
     }
-    if (acc_opencl_devices[device_id] != current_id) {
-      cl_context_properties properties[] = {
-        CL_CONTEXT_PLATFORM, (cl_context_properties)acc_opencl_platforms[device_id],
-        CL_CONTEXT_INTEROP_USER_SYNC, CL_FALSE, /* TODO */
-        0 /* end of properties */
-      };
-      if (NULL != acc_opencl_context) {
-        ACC_OPENCL_CHECK(clReleaseContext(acc_opencl_context),
-          "failed to release OpenCL context", result);
-      }
+    acc_opencl_context = clCreateContext(properties,
+      1/*num_devices*/, acc_opencl_devices + device_id,
+      NULL/*pfn_notify*/, NULL/* user_data*/,
+      &result);
+    if (CL_INVALID_VALUE == result) { /* retry */
+      n = sizeof(properties) / sizeof(*properties);
+      assert(3 <= n);
+      properties[n-3] = 0;
       acc_opencl_context = clCreateContext(properties,
         1/*num_devices*/, acc_opencl_devices + device_id,
         NULL/*pfn_notify*/, NULL/* user_data*/,
         &result);
-      if (CL_INVALID_VALUE == result) { /* retry */
-        n = sizeof(properties) / sizeof(*properties);
-        assert(3 <= n);
-        properties[n-3] = 0;
-        acc_opencl_context = clCreateContext(properties,
-          1/*num_devices*/, acc_opencl_devices + device_id,
-          NULL/*pfn_notify*/, NULL/* user_data*/,
-          &result);
-      }
-      ACC_OPENCL_CHECK(result, "failed to create OpenCL context", result);
     }
-  }
-  else {
-    result = EXIT_FAILURE;
+    ACC_OPENCL_CHECK(result, "failed to create OpenCL context", result);
   }
   ACC_OPENCL_RETURN(result);
 }

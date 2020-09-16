9834e95051af4f732e853f2c66eb6d36116a856b
Hans Pabst <hans.pabst@intel.com>
2020-09-16 18:54:06 +0200

    Further correction for acc_dev_mem_info.

--- src/acc/opencl/acc_opencl_mem.c
+++ src/acc/opencl/acc_opencl_mem.c
@@ -245,56 +245,55 @@ int acc_dev_mem_info(size_t* mem_free, size_t* mem_total)
     ? acc_get_ndevices(&ndevices) : EXIT_FAILURE;
   if (EXIT_SUCCESS == result) {
     size_t size_free = 0, size_total = 0;
-    /* There is no OpenMP function that returns memory statistics of a device.
-     * Instead, the free/available host memory divided by the number of devices
-     * is used as a proxy value.
-     */
-    if (0 < ndevices) {
+
+
+
+
+
 #if defined(_WIN32)
-      MEMORYSTATUSEX mem_status;
-      mem_status.dwLength = sizeof(mem_status);
-      if (GlobalMemoryStatusEx(&mem_status)) {
-        size_total = (size_t)mem_status.ullTotalPhys;
-        size_free = (size_t)mem_status.ullAvailPhys;
-      }
-      else result = EXIT_FAILURE;
+    MEMORYSTATUSEX mem_status;
+    mem_status.dwLength = sizeof(mem_status);
+    if (GlobalMemoryStatusEx(&mem_status)) {
+      size_total = (size_t)mem_status.ullTotalPhys;
+      size_free = (size_t)mem_status.ullAvailPhys;
+    }
+    else result = EXIT_FAILURE;
 #else
 # if defined(_SC_PAGE_SIZE)
-      const long page_size = sysconf(_SC_PAGE_SIZE);
+    const long page_size = sysconf(_SC_PAGE_SIZE);
 # else
-      const long page_size = 4096;
+    const long page_size = 4096;
 # endif
 # if defined(__linux__)
 #   if defined(_SC_AVPHYS_PAGES)
-      const long pages_free = sysconf(_SC_AVPHYS_PAGES);
+    const long pages_free = sysconf(_SC_AVPHYS_PAGES);
 #   else
-      const long pages_free = 0;
+    const long pages_free = 0;
 #   endif
 #   if defined(_SC_PHYS_PAGES)
-      const long pages_total = sysconf(_SC_PHYS_PAGES);
+    const long pages_total = sysconf(_SC_PHYS_PAGES);
 #   else
-      const long pages_total = pages_free;
+    const long pages_total = pages_free;
 #   endif
 # else
-      /*const*/ size_t size_pages_free = sizeof(const long), size_pages_total = sizeof(const long);
-      long pages_free = 0, pages_total = 0;
-      ACC_OPENCL_EXPECT(0, sysctlbyname("vm.page_free_count", &pages_free, &size_pages_free, NULL, 0));
-      if (0 < page_size && 0 == sysctlbyname("hw.memsize", &pages_total, &size_pages_total, NULL, 0)) {
-        pages_total /= page_size;
-      }
-      else pages_total = pages_free;
+    /*const*/ size_t size_pages_free = sizeof(const long), size_pages_total = sizeof(const long);
+    long pages_free = 0, pages_total = 0;
+    ACC_OPENCL_EXPECT(0, sysctlbyname("vm.page_free_count", &pages_free, &size_pages_free, NULL, 0));
+    if (0 < page_size && 0 == sysctlbyname("hw.memsize", &pages_total, &size_pages_total, NULL, 0)) {
+      pages_total /= page_size;
+    }
+    else pages_total = pages_free;
 # endif
-      if (0 < page_size && 0 <= pages_free && 0 <= pages_total && pages_free <= pages_total) {
-        const size_t size_page = (size_t)page_size;
-        size_total = size_page * (size_t)pages_total;
-        size_free  = size_page * (size_t)pages_free;
-      }
-      else result = EXIT_FAILURE;
+    if (0 < page_size && 0 <= pages_free && 0 <= pages_total) {
+      const size_t size_page = (size_t)page_size;
+      size_total = size_page * (size_t)pages_total;
+      size_free  = size_page * (size_t)pages_free;
+    }
+    else result = EXIT_FAILURE;
 #endif
-      if (EXIT_SUCCESS == result) {
-        size_total /= ndevices;
-        size_free  /= ndevices;
-      }
+    if (0 < ndevices) {
+      size_total /= ndevices;
+      size_free  /= ndevices;
     }
     if (size_free <= size_total) { /* EXIT_SUCCESS != result is ok */
       if (NULL != mem_total) *mem_total = size_total;

a223932b3651d8c7cc056f99e85891a9567af8b4
Hans Pabst <hans.pabst@intel.com>
2020-10-18 10:44:24 +0200

    Load and build OpenCL code (transpose). Note, acc_opencl_kernel is not yet implemented in the BE. Further, source code is meant to be embedded (resource) into the C code later (build system).

--- src/acc/opencl_smm/acc_opencl_smm_transpose.c
+++ src/acc/opencl_smm/acc_opencl_smm_transpose.c
@@ -14,6 +14,11 @@
 extern "C" {
 #endif
 
+const char* acc_opencl_batchtrans_source[] = {
+  NULL
+};
+
+
 int acc_opencl_dbatchtrans(const int* dev_trs_stack, int offset, int stack_size,
   double* dev_data, int m, int n, int max_kernel_dim, void* stream)
 {
@@ -29,28 +34,47 @@ int acc_opencl_dbatchtrans(const int* dev_trs_stack, int offset, int stack_size,
   key.m = m; key.n = n; /* initialize key */
   config = (config_t*)libxsmm_xdispatch(&key, sizeof(key));
   if (NULL == config) {
+    FILE *const file = acc_opencl_source_open("transpose.cl", "../../exts/dbcsr/src/acc/opencl_smm/kernel");
     const char *const envnt = getenv("ACC_OPENCL_TRANS_NT");
     const size_t nt = (NULL == envnt ? local_work_size : ((size_t)atoi(envnt)));
     config_t c;
-    c.kernel = NULL; /* TODO: create kernel */
-    c.nthreads = LIBXSMM_MAX(nt, size);
+    assert(NULL != acc_opencl_batchtrans_source);
+    if (NULL != file) {
+      char* lines[50];
+      const int nlines = acc_opencl_source(file, lines, sizeof(lines) / sizeof(*lines), 1/*cleanup*/);
+      fclose(file);
+      result = (0 < nlines ? acc_opencl_kernel((const char**)lines, "build_options", &c.kernel) : EXIT_FAILURE);
+    }
+    if (EXIT_FAILURE == result
+      && sizeof(*acc_opencl_batchtrans_source) <= (sizeof(acc_opencl_batchtrans_source))
+      && NULL != *acc_opencl_batchtrans_source)
+    {
+      result = acc_opencl_kernel(acc_opencl_batchtrans_source, "build_options", &c.kernel);
+    }
+    else {
+      result = EXIT_FAILURE;
+    }
     if (EXIT_SUCCESS == result) {
       size_t preferred_multiple;
       result = acc_opencl_wgsize(c.kernel, &preferred_multiple);
-      if (EXIT_SUCCESS == result) c.nthreads = LIBXSMM_UP(c.nthreads, preferred_multiple);
+      if (EXIT_SUCCESS == result) {
+        c.nthreads = LIBXSMM_UP(LIBXSMM_MAX(nt, size), preferred_multiple);
+        config = (config_t*)libxsmm_xregister(&key, sizeof(key), sizeof(c), &c);
+      }
     }
-    config = (config_t*)libxsmm_xregister(&key, sizeof(key), sizeof(c), &c);
   }
-  assert(NULL != config);
-  ACC_OPENCL_CHECK(clSetKernelArg(config->kernel, 0, sizeof(cl_mem), ACC_OPENCL_MEM(dev_trs_stack)),
-    "set batch-list argument of transpose kernel", result);
-  ACC_OPENCL_CHECK(clSetKernelArg(config->kernel, 1, sizeof(int), &offset),
-    "set offset argument of transpose kernel", result);
-  ACC_OPENCL_CHECK(clSetKernelArg(config->kernel, 2, sizeof(cl_mem), ACC_OPENCL_MEM(dev_data)),
-    "set matix-data argument of transpose kernel", result);
-  ACC_OPENCL_CHECK(clEnqueueNDRangeKernel(*ACC_OPENCL_STREAM(stream), config->kernel, 1/*work_dim*/,
-    NULL, &global_work_size, &config->nthreads, 0, NULL, NULL),
-    "launch transpose kernel", result);
+  if (EXIT_SUCCESS == result) {
+    assert(NULL != config);
+    ACC_OPENCL_CHECK(clSetKernelArg(config->kernel, 0, sizeof(cl_mem), ACC_OPENCL_MEM(dev_trs_stack)),
+      "set batch-list argument of transpose kernel", result);
+    ACC_OPENCL_CHECK(clSetKernelArg(config->kernel, 1, sizeof(int), &offset),
+      "set offset argument of transpose kernel", result);
+    ACC_OPENCL_CHECK(clSetKernelArg(config->kernel, 2, sizeof(cl_mem), ACC_OPENCL_MEM(dev_data)),
+      "set matix-data argument of transpose kernel", result);
+    ACC_OPENCL_CHECK(clEnqueueNDRangeKernel(*ACC_OPENCL_STREAM(stream), config->kernel, 1/*work_dim*/,
+      NULL, &global_work_size, &config->nthreads, 0, NULL, NULL),
+      "launch transpose kernel", result);
+  }
   return result;
 }
 

4a2f6605baad9449ac5c984f1c547bdc1e0113b2
Hans Pabst <hans.pabst@intel.com>
2020-10-19 10:05:31 +0200

    Initialize data.

--- src/acc/opencl_smm/acc_opencl_trans_bench.c
+++ src/acc/opencl_smm/acc_opencl_trans_bench.c
@@ -8,39 +8,57 @@
  *------------------------------------------------------------------------------------------------*/
 #include "acc_opencl_smm.h"
 
+#if !defined(ELEM_TYPE)
+# define ELEM_TYPE double
+#endif
+#if !defined(SHUFFLE) && 0
+# define SHUFFLE
+#endif
+
 
 int main(int argc, char* argv[])
 {
-  typedef double T;
   const int stack_size = (1 < argc ? atoi(argv[1]) : 30000);
   const int m = (2 < argc ? atoi(argv[2]) : 23);
   const int n = (3 < argc ? atoi(argv[3]) : m);
   const int max_kernel_dim = 80, offset = 0;
+  const size_t size = ACC_OPENCL_UP2(sizeof(ELEM_TYPE) * m * n, ACC_OPENCL_CACHELINE_NBYTES);
+#if defined(SHUFFLE)
+  const size_t shuffle = libxsmm_shuffle((unsigned int)stack_size);
+#endif
 
-  void *host_mem, *dev_mem, *host_data, *dev_data;
+  int *host_mem, *dev_mem;
+  ELEM_TYPE *host_data, *dev_data;
   int result = EXIT_SUCCESS;
-  int priomin, priomax;
+  int priomin, priomax, i;
   void *stream = NULL;
 
   libxsmm_timer_tickint start;
+  const double scale = 1.0 / stack_size;
   double duration;
 
   acc_init();
   acc_stream_priority_range(&priomin, &priomax);
   acc_stream_create(&stream, "stream", (priomin + priomax) / 2);
 
-  acc_host_mem_allocate(&host_mem, stack_size * sizeof(int), stream);
-  acc_dev_mem_allocate(&dev_mem, stack_size * sizeof(int));
+  acc_host_mem_allocate((void**)&host_data, size * stack_size, stream);
+  acc_dev_mem_allocate((void**)&dev_data, size * stack_size);
+  for (i = 0; i < stack_size; ++i) { /* initialize stack of matrices */
+    LIBXSMM_MATINIT(ELEM_TYPE, i/*seed*/, host_data + size * i, m, n, m/*ld*/, scale);
+  }
+  acc_memcpy_h2d(host_data, dev_data, size * stack_size, stream);
 
-  /* TODO: initialize matrix stack */
-
-  acc_host_mem_allocate(&host_data, stack_size * m * n * sizeof(T), stream);
-  acc_dev_mem_allocate(&dev_data, stack_size * m * n * sizeof(T));
-
-  /* TODO: initialize matrices */
-
-  acc_memcpy_h2d(host_mem, dev_mem, stack_size * sizeof(int), stream);
-  acc_memcpy_h2d(host_data, dev_data, stack_size * m * n * sizeof(T), stream);
+  acc_host_mem_allocate((void**)&host_mem, sizeof(int) * stack_size, stream);
+  acc_dev_mem_allocate((void**)&dev_mem, sizeof(int) * stack_size);
+  for (i = 0; i < stack_size; ++i) { /* initialize stack of matrices */
+#if defined(SHUFFLE)
+    const size_t j = size * (i * shuffle) % stack_size;
+#else
+    const size_t j = size * i;
+#endif
+    host_mem[i] = j;
+  }
+  acc_memcpy_h2d(host_mem, dev_mem, sizeof(int) * stack_size, stream);
 
   start = libxsmm_timer_tick();
   result = libsmm_acc_transpose((const int*)dev_mem, offset, stack_size,

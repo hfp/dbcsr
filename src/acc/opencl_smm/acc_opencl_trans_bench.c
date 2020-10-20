98a1180f6a69b7cde1055457cd7316aea02982fb
Hans Pabst <hans.pabst@intel.com>
2020-10-20 09:17:56 +0200

    Improved CHECK macro.

--- src/acc/opencl_smm/acc_opencl_trans_bench.c
+++ src/acc/opencl_smm/acc_opencl_trans_bench.c
@@ -26,7 +26,8 @@
 #endif
 
 #define ROUNDUP2(N, NPOT) ((((unsigned long long)N) + ((NPOT) - 1)) & ~((NPOT) - 1))
-#define CHECK(EXPR) if (EXIT_SUCCESS != result || EXIT_SUCCESS != (result = (EXPR))) assert(0)
+#define CHECK(EXPR, RPTR) if ((NULL != (RPTR) && EXIT_SUCCESS != *((const int*)(RPTR))) || \
+  EXIT_SUCCESS != (NULL != (RPTR) ? (*((int*)(RPTR)) = (EXPR)) : (EXPR))) assert(0)
 
 
 static void init(int seed, ELEM_TYPE* dst, int nrows, int ncols, int ld, double scale) {
@@ -67,20 +68,20 @@ int main(int argc, char* argv[])
   const double scale = 1.0 / stack_size;
   double duration;
 
-  CHECK(acc_init());
-  CHECK(acc_stream_priority_range(&priomin, &priomax));
-  CHECK(acc_stream_create(&stream, "stream", (priomin + priomax) / 2));
+  CHECK(acc_init(), &result);
+  CHECK(acc_stream_priority_range(&priomin, &priomax), &result);
+  CHECK(acc_stream_create(&stream, "stream", (priomin + priomax) / 2), &result);
 
-  CHECK(acc_host_mem_allocate((void**)&host_data, sizeof(ELEM_TYPE) * mn * stack_size, stream));
-  CHECK(acc_dev_mem_allocate((void**)&dev_data, sizeof(ELEM_TYPE) * mn * stack_size));
-  CHECK(acc_stream_sync(stream));
+  CHECK(acc_host_mem_allocate((void**)&host_data, sizeof(ELEM_TYPE) * mn * stack_size, stream), &result);
+  CHECK(acc_dev_mem_allocate((void**)&dev_data, sizeof(ELEM_TYPE) * mn * stack_size), &result);
+  CHECK(acc_stream_sync(stream), &result);
   for (i = 0; i < stack_size; ++i) { /* initialize stack of matrices */
     init(i/*seed*/, host_data + mn * i, m, n, m/*ld*/, scale);
   }
-  CHECK(acc_memcpy_h2d(host_data, dev_data, sizeof(ELEM_TYPE) * mn * stack_size, stream));
+  CHECK(acc_memcpy_h2d(host_data, dev_data, sizeof(ELEM_TYPE) * mn * stack_size, stream), &result);
 
-  CHECK(acc_host_mem_allocate((void**)&host_mem, sizeof(int) * stack_size, stream));
-  CHECK(acc_dev_mem_allocate((void**)&dev_mem, sizeof(int) * stack_size));
+  CHECK(acc_host_mem_allocate((void**)&host_mem, sizeof(int) * stack_size, stream), &result);
+  CHECK(acc_dev_mem_allocate((void**)&dev_mem, sizeof(int) * stack_size), &result);
   for (i = 0; i < stack_size; ++i) { /* initialize indexes */
 #if defined(SHUFFLE)
     j = (int)(mn * (i * shuffle) % stack_size);
@@ -89,21 +90,21 @@ int main(int argc, char* argv[])
 #endif
     host_mem[i] = j;
   }
-  CHECK(acc_memcpy_h2d(host_mem, dev_mem, sizeof(int) * stack_size, stream));
+  CHECK(acc_memcpy_h2d(host_mem, dev_mem, sizeof(int) * stack_size, stream), &result);
 
   /* warmup execution and prebuild JIT kernels */
-  CHECK(libsmm_acc_transpose(dev_mem, offset, stack_size,
-    dev_data, dbcsr_type_real_8, m, n, MAX_KERNEL_DIM, stream));
-  CHECK(acc_stream_sync(stream));
+  CHECK(libsmm_acc_transpose(dev_mem, offset, stack_size, dev_data,
+    dbcsr_type_real_8, m, n, MAX_KERNEL_DIM, stream), &result);
+  CHECK(acc_stream_sync(stream), &result);
 #if defined(__LIBXSMM)
   start = libxsmm_timer_tick();
 #endif
   for (r = 0; r < neven; ++r) {
-    CHECK(libsmm_acc_transpose(dev_mem, offset, stack_size,
-      dev_data, dbcsr_type_real_8, m, n, MAX_KERNEL_DIM, stream));
+    CHECK(libsmm_acc_transpose(dev_mem, offset, stack_size, dev_data,
+      dbcsr_type_real_8, m, n, MAX_KERNEL_DIM, stream), &result);
   }
 #if defined(__LIBXSMM)
-  CHECK(acc_stream_sync(stream));
+  CHECK(acc_stream_sync(stream), &result);
   duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
 #endif
   assert(0 < neven);
@@ -111,8 +112,9 @@ int main(int argc, char* argv[])
 #if defined(__LIBXSMM)
   { /* transfer result from device back to host for validation */
     unsigned int nerrors = 0;
-    CHECK(acc_memcpy_d2h(dev_data, host_data, sizeof(ELEM_TYPE) * mn * stack_size, stream));
-    CHECK(acc_stream_sync(stream));
+    CHECK(acc_memcpy_d2h(dev_data, host_data,
+      sizeof(ELEM_TYPE) * mn * stack_size, stream), &result);
+    CHECK(acc_stream_sync(stream), &result);
     for (i = 0; i < stack_size; ++i) { /* initialize stack of matrices */
       ELEM_TYPE matrix[MAX_KERNEL_DIM*MAX_KERNEL_DIM];
       init(i/*seed*/, matrix, m, n, m/*ld*/, scale);
@@ -125,11 +127,11 @@ int main(int argc, char* argv[])
     if (0 != nerrors) result = EXIT_FAILURE;
   }
 #endif
-  CHECK(acc_host_mem_deallocate(host_data, stream));
-  CHECK(acc_host_mem_deallocate(host_mem, stream));
-  CHECK(acc_dev_mem_deallocate(dev_data));
-  CHECK(acc_dev_mem_deallocate(dev_mem));
-  CHECK(acc_stream_destroy(stream));
+  CHECK(acc_host_mem_deallocate(host_data, stream), NULL);
+  CHECK(acc_host_mem_deallocate(host_mem, stream), NULL);
+  CHECK(acc_dev_mem_deallocate(dev_data), NULL);
+  CHECK(acc_dev_mem_deallocate(dev_mem), NULL);
+  CHECK(acc_stream_destroy(stream), NULL);
 
   return result;
 }

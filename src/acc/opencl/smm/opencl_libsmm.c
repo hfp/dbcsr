749d4a2932165f2603d380149cb82afa03af2332
Hans Pabst <hans.pabst@intel.com>
2021-01-05 16:14:27 +0100

    Reverted to scalar atomics.

--- src/acc/opencl/smm/opencl_libsmm.c
+++ src/acc/opencl/smm/opencl_libsmm.c
@@ -365,21 +365,19 @@ int libsmm_acc_process(const int* host_param_stack, const int* dev_param_stack, 
           const char *const env_atomics = getenv("OPENCL_LIBSMM_SMM_ATOMICS");
           const char *atomic_cmpxchg = NULL, *atomic_xchg = NULL;
           const char *atomic_type = NULL, *typename = NULL;
-          const char *atomics_1 = NULL, *atomics_m = NULL;
+          const char *atomics = NULL;
           if (NULL == env_atomics || '0' != *env_atomics) {
             if ((NULL == env_atomics && EXIT_SUCCESS != acc_opencl_device_vendor(active_device, "nvidia"))
               || NULL != acc_opencl_stristr(env_atomics, "cmpxchg"))
             {
-              atomics_1 = "atomic_add1_global_cmpxchg(A,B)";
-              atomics_m = "atomic_addm_global_cmpxchg(A,B)";
+              atomics = "atomic_add_global_cmpxchg(A,B)";
             }
             else {
-              atomics_1 = "atomic_add1_global_xchg(A,B)";
-              atomics_m = "atomic_addm_global_xchg(A,B)";
+              atomics = "atomic_add_global_xchg(A,B)";
             }
           }
           else {
-            atomics_1 = atomics_m = "*(A)+=(B)";
+            atomics = "*(A)+=(B)";
           }
           assert(NULL != active_device);
           switch (datatype) {
@@ -409,21 +407,17 @@ int libsmm_acc_process(const int* host_param_stack, const int* dev_param_stack, 
             const char *const build_setup =
               "%s -cl-fast-relaxed-math -cl-no-signed-zeros -cl-denorms-are-zero"
               " -DGLOBAL=%s -DFN=%s -DSM=%i -DSN=%i -DSK=%i -DVM=%i -DVN=%i -DVK=%i"
-              " -DT=%s -DTVM=%s%i -DTVN=%s%i -DTVK=%s%i"
-              " -DTA=\"%s\" -DTAM=\"%s%i\" -DTAN=\"%s%i\" -DTAK=\"%s%i\""
-              " -DCMPXCHG=%s -DXCHG=%s"
-              " -D\"ATOMIC_ADD1_GLOBAL(A,B)=%s\""
-              " -D\"ATOMIC_ADDM_GLOBAL(A,B)=%s\"";
+              " -DT=%s -DTM=%s%i -DTN=%s%i -DTK=%s%i -DTA=\"%s\""
+              " -DCMPXCHG=%s -DXCHG=%s -D\"ATOMIC_ADD_GLOBAL(A,B)=%s\"";
             const int vm = LIBXSMM_LO2POT(LIBXSMM_MIN(m_max, 16));
             const int vn = LIBXSMM_LO2POT(LIBXSMM_MIN(n_max, 16));
             const int vk = LIBXSMM_LO2POT(LIBXSMM_MIN(k_max, 16));
-            assert(NULL != atomics_1 && NULL != atomics_m);
+            assert(NULL != atomics);
             nchar = ACC_OPENCL_SNPRINTF(build_options, sizeof(build_options), build_setup,
               (NULL == env_options || '\0' == *env_options) ? "" : env_options,
               EXIT_SUCCESS != opencl_libsmm_use_cmem(active_device) ? "global" : "constant", fname,
               m_max, n_max, k_max, vm, vn, vk, typename, typename, vm, typename, vn, typename, vk,
-              atomic_type, atomic_type, vm, atomic_type, vn, atomic_type, vk,
-              atomic_cmpxchg, atomic_xchg, atomics_1, atomics_m);
+              atomic_type, atomic_cmpxchg, atomic_xchg, atomics);
             if (0 < nchar && (int)sizeof(build_options) > nchar) {
               config_t new_config;
 #if defined(OPENCL_SOURCE_MULTIPLY)

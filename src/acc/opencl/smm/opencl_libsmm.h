27c8755762a21f2c06b1b714f723350bf628938b
Hans Pabst <hans.pabst@intel.com>
2021-05-11 08:46:59 +0200

    Attempt to load device-specific tuned parameters.

--- src/acc/opencl/smm/opencl_libsmm.h
+++ src/acc/opencl/smm/opencl_libsmm.h
@@ -89,11 +89,14 @@ typedef struct opencl_libsmm_perfest_t {
 /** If buffers are hinted for non-concurrent writes aka "OpenCL constant". */
 int opencl_libsmm_use_cmem(cl_device_id device);
 
-/* Tokenize parambuf and initialize key/value pair. */
+/** Tokenize parambuf and initialize key/value pair. */
 int opencl_libsmm_read_params(char* parambuf,
   opencl_libsmm_smmkey_t* key, opencl_libsmm_smm_t* value,
   opencl_libsmm_perfest_t* perfest,
   char* const* device);
+
+/** Get active device and configuration name. */
+int opencl_libsmm_device(void* stream, cl_device_id* device, const char** config);
 
 #if defined(OPENCL_LIBSMM_DEBUG) && defined(_DEBUG)
 void opencl_libsmm_print_matrix(FILE* ostream, const char* label,

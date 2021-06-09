f76487d5803f710e1b1b2f760b6ab3fa9dbeaf1d
Hans Pabst <hans.pabst@intel.com>
2021-06-09 16:06:32 +0200

    Apply type-id (TN) in a more generic fashion.

--- src/acc/opencl/smm/opencl_libsmm.c
+++ src/acc/opencl/smm/opencl_libsmm.c
@@ -892,7 +892,7 @@ int libsmm_acc_process(const int* host_param_stack, const int* dev_param_stack, 
             case dbcsr_type_real_8: {
               extensions = "cl_khr_fp64 cl_khr_int64_base_atomics";
               if (EXIT_SUCCESS == c_dbcsr_acc_opencl_device_ext(active_device, &extensions, 1)) {
-                atomic_ops = "-DTA=long -DTN=3 -DCMPXCHG=atom_cmpxchg -DXCHG=atom_xchg";
+                atomic_ops = "-DTA=long -DCMPXCHG=atom_cmpxchg -DXCHG=atom_xchg";
                 tname = "double";
                 fname[0] = 'd';
               }
@@ -900,7 +900,7 @@ int libsmm_acc_process(const int* host_param_stack, const int* dev_param_stack, 
             case dbcsr_type_real_4: {
               extensions = "cl_khr_global_int32_base_atomics";
               if (EXIT_SUCCESS == c_dbcsr_acc_opencl_device_ext(active_device, &extensions, 1)) {
-                atomic_ops = "-DTA=int -DTN=1 -DCMPXCHG=atomic_cmpxchg -DXCHG=atomic_xchg";
+                atomic_ops = "-DTA=int -DCMPXCHG=atomic_cmpxchg -DXCHG=atomic_xchg";
                 tname = "float";
                 fname[0] = 's';
               }
@@ -989,11 +989,11 @@ int libsmm_acc_process(const int* host_param_stack, const int* dev_param_stack, 
                 assert(1 <= bs && 0 < bm && 0 < bn && NULL != atomic_expr);
                 nchar = ACC_OPENCL_SNPRINTF(build_options, sizeof(build_options),
                   "%s -cl-fast-relaxed-math -cl-no-signed-zeros -cl-denorms-are-zero -DFMA=fma"
-                  " -DGLOBAL=%s -DFN=%s -DSM=%i -DSN=%i -DSK=%i -DBM=%i -DBN=%i -DBS=%i -DT=%s"
+                  " -DGLOBAL=%s -DFN=%s -DSM=%i -DSN=%i -DSK=%i -DBM=%i -DBN=%i -DBS=%i -DT=%s -DTN=%i"
                   " %s -D\"ATOMIC_ADD_GLOBAL(A,B)=%s\" %s",
                   (NULL == env_options || '\0' == *env_options) ? "" : env_options,
                   EXIT_SUCCESS != opencl_libsmm_use_cmem(active_device) ? "global" : "constant",
-                  fname, m_max, n_max, k_max, bm, bn, bs, tname, atomic_ops, atomic_expr,
+                  fname, m_max, n_max, k_max, bm, bn, bs, tname, datatype, atomic_ops, atomic_expr,
                   NULL == atomic_expr2 ? "" : atomic_expr2);
                 if (0 >= nchar || (int)sizeof(build_options) <= nchar) result = EXIT_FAILURE;
               }

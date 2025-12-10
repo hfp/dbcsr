63bc6b4fd26d3d23727413b038f4772eefd97c60
Hans Pabst <hans.pabst@intel.com>
2025-12-10 15:01:48 +0100

    Adjusted XHINTS.

--- src/acc/opencl/acc_opencl_mem.c
+++ src/acc/opencl/acc_opencl_mem.c
@@ -316,9 +316,9 @@ int c_dbcsr_acc_host_mem_allocate(void** host_mem, size_t nbytes, void* stream) 
         if (NULL == host_ptr) {
           mapped = clEnqueueMapBuffer(str->queue, memory, CL_TRUE /*always block*/,
 #  if defined(ACC_OPENCL_XHINTS) && (defined(CL_VERSION_1_2) || defined(CL_MAP_WRITE_INVALIDATE_REGION))
-            (8 & c_dbcsr_acc_opencl_config.xhints) ? CL_MAP_WRITE_INVALIDATE_REGION :
+            (16 & c_dbcsr_acc_opencl_config.xhints) ? CL_MAP_WRITE_INVALIDATE_REGION :
 #  endif
-                                                   (CL_MAP_READ | CL_MAP_WRITE),
+                                                    (CL_MAP_READ | CL_MAP_WRITE),
             0 /*offset*/, nbytes, 0, NULL, NULL, &result);
         }
         assert(EXIT_SUCCESS == result || NULL == mapped);
@@ -490,7 +490,7 @@ int c_dbcsr_acc_dev_mem_allocate(void** dev_mem, size_t nbytes) {
 #  if defined(ACC_OPENCL_XHINTS)
       const int devuid = devinfo->uid;
       const int try_flag =
-        ((0 != (16 & c_dbcsr_acc_opencl_config.xhints) && 0 != devinfo->intel && 0 == devinfo->unified &&
+        ((0 != (8 & c_dbcsr_acc_opencl_config.xhints) && 0 != devinfo->intel && 0 == devinfo->unified &&
            ((0x4905 == devuid || 0x020a == devuid || (0x0bd0 <= devuid && 0x0bdb >= devuid)) || NULL != (ACC_OPENCL_XHINTS)))
             ? (1u << 22)
             : 0);

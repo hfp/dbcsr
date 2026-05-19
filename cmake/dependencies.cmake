93ff69f57d60132fd30d01f0d8c84aa50c071b8e
Hans Pabst <hans.pabst@intel.com>
2026-05-19 11:44:06 +0200

    Zero config

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,7 @@
+set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
+set(LIBXS_GIT_TAG "97e9e24d9c2dabdb6d7e31d6de8812f233f17af3")
+
+set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
+set(LIBXSTREAM_GIT_TAG "aaf0915cf2e17245f853206b4d45b9195d72a716")
+
+set(LIBXSMM_GIT_TAG "c14cbc6f8bc7964f8c5190a3a16b8cace03e5889")

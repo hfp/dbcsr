19528e82e2ffe894d0acbc996297065559a69318
Hans Pabst <hans.pabst@intel.com>
2026-05-20 14:03:19 +0200

    Fixed build and deps

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,7 @@
 set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
-set(LIBXS_GIT_TAG "3225963402e1c806df622c5f4bbfd5929919dc68")
+set(LIBXS_GIT_TAG "57dfd23b349aa703c49d59cca615b3dd2c558a52")
 
 set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "45c369b5d91bc2819a5b97e8edc8325a8c699f67")
+set(LIBXSTREAM_GIT_TAG "ae7fe2e87494ff865c5d03a9402ebf0044555b5a")
 
 set(LIBXSMM_GIT_TAG "c14cbc6f8bc7964f8c5190a3a16b8cace03e5889")

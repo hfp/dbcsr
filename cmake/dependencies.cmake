a4de1c8a6219519a0141df744bd314583f6b122d
Hans Pabst <hans.pabst@intel.com>
2026-05-25 21:06:01 +0200

    Updated LIBXS/LIBXSTREAM

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,7 @@
 set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
-set(LIBXS_GIT_TAG "051ad3eac78fcf8b2126843a9bfbfbe1815a92c2")
+set(LIBXS_GIT_TAG "1d776c091fb41d4aa7bc92335f6387878e1a9db6")
 
 set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "d03b33e5dcb597ec4c80d0cd644de1cab4725da3")
+set(LIBXSTREAM_GIT_TAG "87bbd69f0cdf6ca23801923ee201f960fb37860f")
 
 set(LIBXSMM_GIT_TAG "c14cbc6f8bc7964f8c5190a3a16b8cace03e5889")

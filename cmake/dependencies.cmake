5799318bc177bd99077e9bb5bee317b11e711c77
Hans Pabst <hans.pabst@intel.com>
2026-06-08 15:09:06 +0200

    Incorporated Growl1234's work

--- cmake/dependencies.cmake
+++
@@ -1,7 +0,0 @@
-set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
-set(LIBXS_GIT_TAG "11348a69c6b8622f9192ec725b291f42384a5ce7")
-
-set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "8375c018d0619bfd9261f67b9ed8b609d8e4ff66")
-
-set(LIBXSMM_GIT_TAG "a85ad8827fd0dc9de88bad12d6d95a4022fc02a0")
